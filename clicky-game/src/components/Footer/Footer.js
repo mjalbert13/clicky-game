748e95f54018c2757e6cc4a75ab33a225a416b68
mjalbert13 <mjalbert13@gmail.com>
2019-08-05 13:37:32 -0400

    added bootstrap and footer compnent

---
+++ clicky-game/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => (
+    <footer className="footer">
+        <div className= "bottom">
+            <ul className= "footer-list">
+                <li><span>Clicky-Game</span></li>
+                <li><span>copyright(c)</span></li>
+                <li><span>Github</span></li>
+            </ul>
+        </div>
+    </footer>
+)
+
+export default Footer;
