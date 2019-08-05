0730f19ad9a77e4fe8f91d3a2b9eefec25dc7aca
mjalbert13 <mjalbert13@gmail.com>
2019-08-05 13:54:12 -0400

    added header component

--- clicky-game/src/components/Footer/Footer.js
+++ clicky-game/src/components/Footer/Footer.js
@@ -11,6 +11,6 @@ const Footer = () => (
             </ul>
         </div>
     </footer>
-)
+);
 
 export default Footer;
---
+++ clicky-game/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+    <div className = "jumbotron jumbotron-fluid">
+        <div className = "container">
+            <h1 className = "display-4">The Click Game</h1>
+            <p className = "lead">Click to play</p>
+        </div>
+    </div>
+);
+
+export default Header;
