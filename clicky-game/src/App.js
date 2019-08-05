17ea1518b7b7c0c5535c4591f8c92a552fc41f0b
mjalbert13 <mjalbert13@gmail.com>
2019-08-05 15:46:18 -0400

    added iamge card

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ImageCard from "./components/ImageCard"
 import './App.css';
 
 function App() {
   return (
     <div className = "container">
     <Header />
+    <ImageCard />
     <Footer />
     </div>
   )
---
+++ clicky-game/src/components/ImageCard/ImageCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageCard =() => (
+    <div className= "card">
+        <div className="image-container">
+            <img className="display-2"></img>
+        </div>
+    </div>
+)
+
+export default ImageCard;
