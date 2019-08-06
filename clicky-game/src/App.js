4b71b01a4db6e11af7c0088eacafe4a6a76699ca
mjalbert13 <mjalbert13@gmail.com>
2019-08-06 13:48:16 -0400

    game working and added basic style

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,18 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component}  from 'react';
+import logo, { ReactComponent } from './logo.svg';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import ImageCard from "./components/ImageCard"
+import Container from './components/Container'
+import cards from './images.json'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className = "container">
-    <Header />
-    <ImageCard />
-    <Footer />
-    </div>
-  )
+class App extends Component {
+  state = {
+    cards,
+    score: 0,
+    highScore: 0
+  }
+
+  gameOver = () => {
+    if(this.state.score > this.state.highScore){
+      this.setState({highScore: this.state.score}, function(){
+        console.log(this.state.highScore);
+      });
+    }
+    this.state.cards.forEach(cards => {
+      cards.count =0;
+    });
+    alert('Game Over!  \nscore :'+ this.state.score);
+    this.setState({score: 0});
+    return true
+  }
+
+  handleClickCount = id => {
+    this.state.cards.find((data,i) => {
+      if(data.id === id) {
+        if(cards[i].count === 0){
+          cards[i].count = cards[i].count +1;
+          this.setState({score: this.state.score +1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true
+        }else{
+          this.gameOver();
+        }
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header score={this.state.score} highScore={this.state.highScore}></Header> 
+        {this.state.cards.map(card => (
+          <ImageCard
+            handleClickCount = {this.handleClickCount}
+            id ={ card.id}
+            key = {card.id}
+            image = {card.image}
+          />
+        ))}
+        <Footer/>
+      </Container>
+      
+    )
+  }
+
 }
 
 export default App;
--- clicky-game/src/components/ImageCard/ImageCard.js
+++ clicky-game/src/components/ImageCard/ImageCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './ImageCard.css'
 
 const ImageCard = props => (
     
-    <div className= "card" onClick={() => props.clickCount(props.id)}>
+    <div className= "card" onClick={() => props.handleClickCount(props.id)}>
         <div className="img-container">
             <img className="display-2" alt={props.id} src={props.image}></img>
         </div>
